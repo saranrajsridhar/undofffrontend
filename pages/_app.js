a1185031eddd6665201d778813e53c43b094e482
killerspace <killerspace368@gmail.com>
2020-06-14 15:42:09 +0530

    frontend update

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import '../static/css/search.css';
+import '../static/css/styles.css';
+import '../pages/aboutus/styles2.css';
+import '../pages/footer/styles3.css';
+
+export default function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+  }
+  
